bb32928b65e156eab864b32c8ff0bd2dc07c8510
Dani Canelo <78096392+danicanelo@users.noreply.github.com>
2023-11-30 19:07:01 +0100

    minor fixes

--- src/auth/services/yunuki.service.ts
+++ src/auth/services/yunuki.service.ts
@@ -11,7 +11,40 @@ export default class YunukiService {
         "Content-Type": "application/json",
       },
     });
+    if (res.status === 404) {
+      return res;
+    }
     const yunuki = await res.json();
     return yunuki;
+  }
+
+  static async feedYunuki() {
+    await fetch(this.apiUrl + "/yunukis/feed", {
+      method: "PUT",
+      headers: {
+        Authorization: "Bearer " + AuthService.getJwt(),
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  static async cleanYunuki() {
+    await fetch(this.apiUrl + "/yunukis/clean", {
+      method: "PUT",
+      headers: {
+        Authorization: "Bearer " + AuthService.getJwt(),
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  static async sleepYunuki() {
+    await fetch(this.apiUrl + "/yunukis/sleep", {
+      method: "PUT",
+      headers: {
+        Authorization: "Bearer " + AuthService.getJwt(),
+        "Content-Type": "application/json",
+      },
+    });
   }
 }
--- src/yunukiStage.js
+++ src/yunukiStage.js
@@ -1,15 +1,11 @@
 import React from "react";
 import YunukiService from "./auth/services/yunuki.service.ts";
-import { Button } from "./core/components/button";
 import { Card } from "./core/components/card";
 import { Navbar } from "./core/components/navbar";
 import { ProgressBar } from "./core/components/progress-bar";
 import { Yunuki } from "./core/components/yunuki";
 import "./core/components/yunuki.css";
 
-const YunukiStageStyles = {
-  ButtonStyle: "mx-5",
-};
 export function YunukiStage() {
   const [yunuki, setYunuki] = React.useState();
 
@@ -55,15 +51,24 @@ export function YunukiStage() {
           <div className="fondo1 is-fullheight column hero is-flex-direction-column is-justify-content-space-around">
             <Yunuki />
             <div className="is-flex is-justify-content-center">
-              <Button type="Info" styles={YunukiStageStyles.ButtonStyle}>
+              <button
+                className="button is-info mx-5"
+                onClick={() => YunukiService.feedYunuki()}
+              >
                 Alimentar
-              </Button>
-              <Button type="Info" styles={YunukiStageStyles.ButtonStyle}>
+              </button>
+              <button
+                className="button is-info mx-5"
+                onClick={() => YunukiService.cleanYunuki()}
+              >
                 Limpiar
-              </Button>
-              <Button type="Info" styles={YunukiStageStyles.ButtonStyle}>
+              </button>
+              <button
+                className="button is-info mx-5"
+                onClick={() => YunukiService.sleepYunuki()}
+              >
                 Dormir
-              </Button>
+              </button>
             </div>
           </div>
         </div>
