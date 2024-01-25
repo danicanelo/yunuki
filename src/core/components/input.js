001f18af48244700a19a4487a056988d7ddacf57
Dani Canelo <danielcanelosoria@gmail.com>
2024-01-25 10:32:39 +0100

    Input props fixed

--- src/core/components/input.js
+++ src/core/components/input.js
@@ -1,30 +1,13 @@
-// En este componente creamos un input para la recepción de datos desde formularios. Recibe una label a modo de "título", un identificador que nos sirve tanto para el id como para el name, un tipo de input, un valor que será el valor que contendrá el input, un placeholder, las instrucciones que debe ejecutar al ser modificado y una longitud mínima
+// En este componente creamos un input para la recepción de datos desde formularios. En este caso, condensamos todas las propiedades que el fichero puede recibir en una única instrucción: ...props. Lo que le decimos con esta instrucción es que obtenga todas las propiedades que sean establecidas a la hora de utilizar el componente, de forma que no sea necesario mencionarlas una por una. Posteriormente las utilizamos a conveniencia (como hacemos en este ejemplo con props.id o props.label) o directamente las volcamos de nuevo con ...props, preocupándonos de añadir las propiedades que queramos "dejar fijas", como por ejemplo hacemos en nuestro ejemplo con className, dado que nos interesa que todos los inputs de nuestra aplicación tenga aplicados los mismos estilos.
 
-export function Input({
-  label,
-  id,
-  type,
-  value,
-  placeholder,
-  onChange,
-  minLength = 0,
-}) {
+export function Input({ ...props }) {
   return (
     <div className="field">
-      <label className="label" htmlFor={id}>
-        {label}
+      <label className="label" htmlFor={props.id}>
+        {props.label}
       </label>
       <div className="control">
-        <input
-          minLength={minLength}
-          className="input"
-          id={id}
-          name={id}
-          type={type}
-          value={value}
-          placeholder={placeholder}
-          onChange={onChange}
-        />
+        <input className="input" name={props.id} {...props} />
       </div>
     </div>
   );
